fe27700233625efe30cb70abad678e564dc9e942
Igor Valerevich <sky323starydrug@yandex.ru>
2026-06-26 10:58:47 +0700

    lesson 2: numbers and strings

---
+++ lesson_2/lesson_2_1.py
@@ -0,0 +1,37 @@
+# задание 1
+a = 10
+b = 3
+x = 2.5
+
+print(type(a), type(x), sep='\n')
+print(a+b, a-b, a*b, a/b, a//b, a%b, a**b, sep="\n")
+
+# задание 2
+first_name = "Иван"
+last_name = "Петров"
+age = 25
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+print(f"Возраст: {age}")
+print(len(full_name))
+print("Иван" in full_name)
+print(ord("A"))
+print("ха" * 3)
+
+empty = ""
+print(empty)
+
+# задание 3
+count = 10
+count += 5
+count -= 3
+count *= 2
+count /= 4
+
+print(count)
+
+number_text ="2312"
+print(type(int(number_text)))
+
